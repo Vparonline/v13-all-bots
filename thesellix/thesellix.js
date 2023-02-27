638fe4124d275919c357ebea258c320eed0836c3
chavodeyeter <74346832+Vparonline@users.noreply.github.com>
2023-02-27 04:01:12 +0300

    Update thesellix.js

---
+++ thesellix/thesellix.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const MongoDBURI = process.env.MONGO_URI || 'mongodb://127.0.0.1/basbasparalarıleylaya';
+
+mongoose.connect(MongoDBURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+});
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/views'));
+
+const index = require('./routes/index');
+app.use('/', index);
+
+
+app.use((req, res, next) => {
+  const err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+app.listen(process.env.PORT || 443, () => {
+  console.log('Express app listening on port 443');
+});
